f5a6b2fd64a259c9e34d3b860546019f90a152be
Lady9696 <leidysuarez55@gmail.com>
2023-11-30 18:26:19 -0500

    cards con props

--- proyecto-final/src/components/Card/card.js
+++ proyecto-final/src/components/Card/card.js
@@ -1,30 +1,22 @@
-import useFetch from "../../hooks/useFetch"
-import { BASE_URL_CATEGORIES } from "../config/list"
+
 import './card.css'
 import { IMAGE_CATEGORIES } from "../config/list"
 
 
-const Card = () => {
+const Card = ({ category }) => {
     //peticiòn para mostrar la categoria 
-    const { data: categories, loading, error } = useFetch(BASE_URL_CATEGORIES);
-
-    if (loading) return <p>Cargando categorías...</p>;
-    if (error) return <p>Error al cargar categorías: {error.message}</p>;
-console.log(categories, 'categorias')
+   
     
 return (
     <div className="container">
-    {categories && categories.map((category, index) => (
-        <div className="card"  key={index}> 
-            <div >
-            <img src={IMAGE_CATEGORIES}/>
-            <p className="category-title">{category}</p> </div>
-                
-
-            </div>
-       
-    ))}
+    <div className="card">
+        <img src={IMAGE_CATEGORIES} alt={category.name}/> {/* Asumiendo que category tiene estas propiedades */}
+        <p>{category}</p>
+    </div>
 </div>
+       
+    
+
 );
 }
 
--- proyecto-final/src/components/Layout/layout.js
+++ proyecto-final/src/components/Layout/layout.js
@@ -1,14 +1,22 @@
 import Card from "../Card/card";
 import './layout.css'
+import useFetch from "../../hooks/useFetch"
+import { BASE_URL_CATEGORIES } from "../config/list"
 
 const Layout = () => {
     
-    
+  const { data: categories, loading, error } = useFetch(BASE_URL_CATEGORIES);
+
+  if (loading) return <p>Cargando categorías...</p>;
+  if (error) return <p>Error al cargar categorías: {error.message}</p>;
+console.log(categories, 'categorias')
+  
 return (
-    <div className="principal-container">
-      
-      <Card></Card>
-      </div>
+  <div className="principal-container">
+  {categories.map((category, index) => (
+    <Card key={index} category={category} />
+  ))}
+</div>
   );
 }
 
