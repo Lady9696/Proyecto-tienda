2dc5cbe03eef3f610643f6050868320217ea51de
Lady9696 <leidysuarez55@gmail.com>
2023-12-08 10:30:31 -0500

    añado categoria

--- proyecto-final/src/App.js
+++ proyecto-final/src/App.js
@@ -1,11 +1,15 @@
-
+import {Routes, Route } from 'react-router-dom';
  import './App.css';
 import Layout from "./components/Layout/layout";
+import LayoutCompra from './components/layoutCompra/layoutCompra';
 function App() {
   return (
-    <div className="App">
-      <Layout/>
-    </div>
+    
+    <Routes>
+       <Route path="/" element={<Layout />} />
+        <Route path="/compra/:categoryName" element={<LayoutCompra />} />
+      </Routes>
+  
   );
 }
 
--- proyecto-final/src/components/Card/card.js
+++ proyecto-final/src/components/Card/card.js
@@ -3,21 +3,42 @@ import './card.css'
 import { IMAGE_CATEGORIES } from "../config/list"
 
 
-const Card = ({ category }) => {
-    //peticiòn para mostrar la categoria 
+const Card = ({ category, onCategoryClick, product, onProductClick, title, price, description}) => {
+    const isProduct = product != null;
    
-    
+    const handleClick = () => {
+        if (isProduct && onProductClick) {
+            onProductClick(product.id);
+        } else if (onCategoryClick) {
+            onCategoryClick(category);
+        }
+    };
 return (
-    <div className="container">
-    <div className="card">
-        <img src={IMAGE_CATEGORIES} alt={category}/> 
-        <p className='category-title'>{category}</p>
-    </div>
-</div>
+    <div className="container" onClick={handleClick}>
+            <div className="card">
+                {isProduct ? (
+                    <>
+                        <img src={product.image} alt={title} />
+                        <p className='title'>{title}</p>
+                        <p className='price'>{price}</p>
+                        <p className='descripcion'>{description}</p>
+                        <p className='category'>{category}</p>
+                         
+                       
+                        {/* Añadir aquí más detalles del producto si es necesario */}
+                    </>
+                ) : (
+                    <>
+                        <img src={IMAGE_CATEGORIES} alt={category} />
+                        <p className='category-title'>{category}</p>
+                    </>
+                )}
+            </div>
+        </div>
+    );
        
     
 
-);
 }
 
 
